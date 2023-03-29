950c5e71a720c9438742d64fb89c1957c68c085f
dariaost19 <dariaost19@gmail.com>
2023-03-28 20:25:34 -0400

    i added the feature sun but there are errors that need to be resolved

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,11 @@ public class Main {
         solar.sun="1";
         solar.moon="2";
         solar.planets="2";
+        FeatureSun sun=new FeatureSun();
+        sun.temperature="1132556647";
+        sun.radius=546633156;
+        sun.color="pink";
+
         
 
     }
