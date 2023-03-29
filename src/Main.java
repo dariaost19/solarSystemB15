49d43506b776d3ed48b9e0685d6f6bb776f2e62e
dariaost19 <dariaost19@gmail.com>
2023-03-28 20:38:08 -0400

    i have successfully resolved the issue and code is ready to be merged into sun branch

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         solar.planets="2";
         FeatureSun sun=new FeatureSun();
         sun.temperature="1132556647";
-        sun.radius=546633156;
+        sun.radius="546633156";
         sun.color="pink";
 
         
